06aba25fe9bb06166d49ccf50f166eb1704d9075
duarte <gabrieldvr@outlook.com>
2023-04-07 15:40:39 -0300

    requisito 4

---
+++ challenges/desafio4.sql
@@ -0,0 +1,11 @@
+SELECT 
+    u.name AS pessoa_usuaria,
+    IF(MAX(h.reproduction_date) >= '2021-01-01',
+        'Ativa',
+        'Inativa') AS status_pessoa_usuaria
+FROM
+    users AS u
+        JOIN
+    history AS h ON h.user_id = u.id
+GROUP BY pessoa_usuaria
+ORDER BY pessoa_usuaria;
