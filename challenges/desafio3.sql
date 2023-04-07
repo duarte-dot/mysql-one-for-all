992e378cd0caf5eb545df2acf8355f5655ef320e
duarte <gabrieldvr@outlook.com>
2023-04-07 14:58:53 -0300

    requisito 3

---
+++ challenges/desafio3.sql
@@ -0,0 +1,12 @@
+SELECT 
+    u.name AS pessoa_usuaria,
+    COUNT(h.song_id) AS musicas_ouvidas,
+    (ROUND(SUM(s.song_duration) / 60, 2)) AS total_minutos
+FROM
+    users AS u
+        JOIN
+    history AS h ON h.user_id = u.id
+        JOIN
+    songs AS s ON s.id = h.song_id
+GROUP BY pessoa_usuaria
+ORDER BY pessoa_usuaria;
