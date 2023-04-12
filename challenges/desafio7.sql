4c8808ec4ce0d142e07186d0691f85c77239347a
duarte <gabrieldvr@outlook.com>
2023-04-12 15:56:13 -0300

    requisito 7

---
+++ challenges/desafio7.sql
@@ -0,0 +1,12 @@
+SELECT 
+    art.name AS artista,
+    alb.name AS album,
+    COUNT(f.artist_id) AS pessoas_seguidoras
+FROM
+    artists AS art
+        JOIN
+    albums AS alb ON art.id = alb.artist_id
+        JOIN
+    following AS f ON f.artist_id = art.id
+GROUP BY art.id , alb.name
+ORDER BY pessoas_seguidoras DESC, artista, album;
