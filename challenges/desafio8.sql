e7d57aca8dc5266412a9124e65a1c1cf0aee4be3
duarte <gabrieldvr@outlook.com>
2023-04-12 16:03:16 -0300

    requisito 8

---
+++ challenges/desafio8.sql
@@ -0,0 +1,9 @@
+SELECT 
+    art.name AS artista, alb.name AS album
+FROM
+    artists AS art
+        JOIN
+    albums AS alb ON art.id = alb.artist_id
+WHERE
+    art.name = 'Elis Regina'
+ORDER BY alb.name;
