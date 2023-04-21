de55a2683f7855823183ca2cf2ffa348af30d9b1
duarte <gabrieldvr@outlook.com>
2023-04-21 15:51:32 -0300

    requisito 11

---
+++ challenges/desafio11.sql
@@ -0,0 +1,11 @@
+SELECT 
+    a.name AS album, COUNT(s.album_id) AS favoritadas
+FROM
+    favorite_songs f
+        INNER JOIN
+    songs s ON s.id = f.song_id
+        INNER JOIN
+    albums a ON s.album_id = a.id
+GROUP BY s.album_id
+ORDER BY COUNT(user_id) DESC
+LIMIT 3;
